486c52c9d7de797fdff8b94d3256ee9d75aa54a4
Daniel Lundström <Daniel.Lundstrom@yh.nackademin.se>
2017-03-20 15:28:18 +0100

    Implemented methods in Translator class

--- src/main/java/se/nackademin/Translator.java
+++ src/main/java/se/nackademin/Translator.java
@@ -10,5 +10,21 @@ package se.nackademin;
  * @author daniel
  */
 public class Translator {
+    private final static String[] NOUNS_ARR = {"en löneförhöjning", "en lönesänkning", "en fotboja", 
+            "en katt", "en hund", "ett hus", "ett barn", "ett elstängsel", "en dator", "ett golv"};
+    private final static String[] VERBS_ARR = {"stor", "liten", "stark", "svag", "mjuk", "hård",
+            "snabb", "vacker", "ljus", "mörk"};
+    private final static String[] ADJECTIVES_ARR = {"springa", "ljuga", "flyga", "se", "vara", "äta",
+            "mäta", "gå", "röra", "resa"};
+    
+    public String getNoun(int number) {
+        return NOUNS_ARR[number];
+    }
+    public String getVerb(int number) {
+        return VERBS_ARR[number];
+    }
+    public String getAdjectives(int number) {
+        return ADJECTIVES_ARR[number];
+    }
     
 }
